42449631ec0fb6c388f771386e6e1cc2bb4a0b99
Melzar Jan Chico <89144070+melzarjanchico@users.noreply.github.com>
2021-11-04 17:44:29 +0800

    Main code commit

---
+++ chico_mp3_numberone.py
@@ -0,0 +1,106 @@
+"""
+    FILE:       chico_mp3_numberone.py
+    ABOUT:      Implements a recursive-descent (predictive) parser for a given grammar.
+
+    NAME:       Melzar Jan E. Chico
+    COURSE:     CMSC124 B
+    DATE:       8 Novemeber 2021
+    TASK:       Machine Problem 3 - Lexical and Syntax Analysis (No. 1)
+
+    NOTICE:     The grammar was modified to remove left recursion/pair disjointedness.
+                Here's the final grammar:
+                    <expr> ::= <term><expr'>
+                    <expr'> ::= +<term><expr'> | -<term><expr'> | e
+                    <term> ::= <factor><term'>
+                    <term'> ::= *<factor><term'> | /<factor><term'> | e
+                    <factor> ::= (<expr>) | <digit>
+                    <digit> ::= 0 | 1 | 2 | 3
+"""
+
+class arithmeticParser:
+    def __init__(self, strLine) -> None:
+        self.__strLine = strLine
+        self.__currChar = None
+        self.__currPos = -1
+        self.__isError = False
+        self.nextChar()
+
+    def nextChar(self):
+        self.__currPos += 1
+
+        if self.__currPos >= len(self.__strLine):
+            # Forced '$' incase user forgot the dollar sign
+            self.__currChar = '$'
+        else:
+            self.__currChar = self.__strLine[self.__currPos]
+
+    def match(self, char):
+        return self.__currChar == char
+
+    def starter(self):
+        self.expression()
+
+        # Checks if the parser read the string entirely
+        if not self.match('$') or (self.match('$') and self.__currPos < len(self.__strLine)-1):
+            self.__isError = True
+
+        return (True if not self.__isError else False)
+
+    ##### PROCEDURES IN THE GRAMMAR RULES BELOW #####
+    def expression(self):
+        self.term()
+        self.expression_prime()
+
+    def expression_prime(self): # note: this procedure can be further simplified.
+        if self.match('+'):     # <expr'> ::= +<term><expr'>
+            self.nextChar()
+            self.term()
+            self.expression_prime()
+        elif self.match('-'):   # <expr'> ::= -<term><expr'>
+            self.nextChar()
+            self.term()
+            self.expression_prime()
+        else:                   # <expr'> ::= e (this can be removed too, just added this for clarity)
+            pass
+
+    def term(self):
+        self.factor()
+        self.term_prime()
+    
+    def term_prime(self):       # note: this procedure can be further simplified.
+        if self.match('*'):     # <term'> ::= *<factor><term'>
+            self.nextChar()
+            self.factor()
+            self.term_prime()
+        elif self.match('/'):   # <term'> ::= /<factor><term'>
+            self.nextChar()
+            self.factor()
+            self.term_prime()
+        else:                   # <term'> ::= e (this can be removed too, just added this for clarity)
+            pass
+
+    def factor(self):
+        if self.match('('):
+            self.nextChar()
+            self.expression()
+            if self.match(')'):
+                self.nextChar()
+            else:
+                self.__isError = True
+        elif self.__currChar in ['0','1','2','3']:  # This could be self.digit(), but it's redundant
+            self.nextChar()
+        else:
+            self.__isError = True
+
+
+def main():
+    inputString = input()
+
+    while inputString != 'exit':
+        # Parser segment
+        parser = arithmeticParser(inputString)
+        print('String is ACCEPTED.\n' if parser.starter() else 'String is NOT ACCEPTED.\n')
+
+        inputString = input()
+
+main()
---
+++ chico_mp3_numbertwo.py
@@ -0,0 +1,97 @@
+"""
+    FILE:       chico_mp3_numbertwo.py
+    ABOUT:      Implements a recursive-descent (predictive) parser for a given grammar.
+
+    NAME:       Melzar Jan E. Chico
+    COURSE:     CMSC124 B
+    DATE:       8 Novemeber 2021
+    TASK:       Machine Problem 3 - Lexical and Syntax Analysis (No. 2)
+
+    NOTICE:     The grammar was modified in accordance "to I see fit that it accepts correct inputs"
+                Here's the final grammar:
+                    <integer> ::= [+|-] <num>
+                    <num> ::= <digit><num'>
+                    <num'> ::= <digit><num'> | .<decimal> | e
+                    <decimal> ::= <digit><decimal'>
+                    <decimal'> ::= <digit><decimal'> | e
+                    <digit> ::= 0 | 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9
+"""
+
+class numberParser:
+    def __init__(self, strLine) -> None:
+        self.__strLine = strLine
+        self.__currChar = None
+        self.__currPos = -1
+        self.__isError = False
+        self.nextChar()
+
+    def nextChar(self):
+        self.__currPos += 1
+
+        if self.__currPos >= len(self.__strLine):
+            # Forced '$' incase user forgot the dollar sign
+            self.__currChar = '$'
+        else:
+            self.__currChar = self.__strLine[self.__currPos]
+
+    def match(self, char):
+        return self.__currChar == char
+
+    def starter(self):
+        self.integer()
+
+        # Checks if the parser read the string entirely
+        if not self.match('$') or (self.match('$') and self.__currPos < len(self.__strLine)-1):
+            self.__isError = True
+
+        return (True if not self.__isError else False)
+
+    ##### PROCEDURES IN THE GRAMMAR RULES BELOW #####
+    def integer(self):
+        if self.match('+') or self.match('-'):
+            self.nextChar()
+        self.number()
+
+    def number(self):
+        self.digit()
+        self.number_prime()
+
+    def number_prime(self):
+        if self.__currChar.isnumeric():
+            self.digit()
+            self.number_prime()
+        elif self.match('.'):
+            self.nextChar()
+            self.decimal()
+        else:
+            pass
+
+    def decimal(self):
+        self.digit()
+        self.decimal_prime()
+
+    def decimal_prime(self):
+        if self.__currChar.isnumeric():
+            self.digit()
+            self.decimal_prime()
+        else:
+            pass
+
+    def digit(self):
+        if self.__currChar in ['0','1','2','3','4','5','6','7','8','9']:
+            self.nextChar()
+        else:
+            self.__isError = True
+
+
+def main():
+    inputString = input()
+
+    while inputString != 'exit':
+        # Parser segment
+        parser = numberParser(inputString)
+        print('String is ACCEPTED.\n' if parser.starter() else 'String is NOT ACCEPTED.\n')
+
+        inputString = input()
+
+main()
